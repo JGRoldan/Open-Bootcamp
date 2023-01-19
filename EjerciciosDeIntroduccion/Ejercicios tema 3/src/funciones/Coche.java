9420c2e8138f456b4864e09efd5e2ac74683eae0
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2023-01-19 12:12:58 -0300

    Add files via upload

---
+++ EjerciciosDeIntroduccion/Ejercicios tema 3/src/funciones/Coche.java
@@ -0,0 +1,19 @@
+package funciones;
+
+public class Coche {
+
+	private int puertas;
+	
+	public Coche(int puertas) {
+		this.puertas = puertas;
+	}
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas() {
+		this.puertas++;
+	}
+	
+}
---
+++ EjerciciosDeIntroduccion/Ejercicios tema 3/src/funciones/Suma.java
@@ -0,0 +1,8 @@
+package funciones;
+
+public class Suma {
+
+	public int suma(int num1, int num2, int num3) {
+		return num1 + num2 + num3;
+	}
+}
