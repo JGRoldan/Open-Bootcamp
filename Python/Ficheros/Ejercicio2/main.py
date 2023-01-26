e07002eb6868f75f46da43ea463a2d53b62b1070
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2023-01-26 10:37:17 -0300

    Update main.py

--- Python/Ficheros/Ejercicio2/main.py
+++ Python/Ficheros/Ejercicio2/main.py
@@ -1,1 +1,23 @@
+import pickle
 
+#Clase sin constructor
+class Vehiculo:
+    ruedas = 4
+    puertas = 5
+    bocina = True
+    
+    def getDatos(self):
+        return f'Ruedas {self.ruedas}, puertas {self.puertas}, bocina {self.bocina}'
+    
+v = Vehiculo()
+
+#Guardar datos en el disco
+f = open('datos.txt', 'wb')
+pickle.dump(v,f)
+f.close()
+
+#Cargar los datos del disco
+f = open('datos.txt', 'rb')
+v1=pickle.load(f)
+f.close()
+print(v1.getDatos())
