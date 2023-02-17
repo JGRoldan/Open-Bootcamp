99dc514f1bfa9eb4ca4dc34a88e15aae7a8f1bcc
JGRDeveloper <jgroldan.dev@gmail.com>
2023-02-17 18:46:40 -0300

    Ejercicio 1

---
+++ JavaBasico/01-Ejercicio.java
@@ -0,0 +1,20 @@
+package ejercicio;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		long l = 958441654L;
+		double d = 10.9d;
+		float f = 5.5f;
+		boolean b = true;
+		String s = "Gaston";
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(b);
+		System.out.println(s);
+	}
+}
